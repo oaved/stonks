6cf3bc37df4b090e57315520533b92f9fdc0cb93
Olof <vedungolof@gmail.com>
2025-01-21 09:37:06 +0100

    change activation function to tanh and modify a lot of vector shapes, network still sucks but does come to a prediction.

--- main.py
+++ main.py
@@ -1,35 +1,10 @@
-from get_stock_data import get_stock_data
-from basic_measurements import calc_moving_average, calc_volatility, add_indicators
-from plots import basic_candlestick_plot, plot_basic_measurements
-from neural_network import NeuralNetwork
-
 import numpy as np
 import pandas as pd
 
-def create_targets(data, margin_per_share=20, horizon=14):
-    window_size = horizon * 10
-    num_targets = len(data["Close"]) - horizon
-    targets = np.zeros((num_targets, 3))  # [buy, hold, sell]
-    training_data = []
-
-    for i in range(window_size, num_targets):
-        training_window = data["Close"].iloc[i - window_size:i + 1]  # Training window
-        training_data.append(training_window.values)
-
-        current_price = float(data["Close"].iloc[i]) 
-        future_price = float(data["Close"].iloc[i + horizon])
-
-        # Calculate target based on price movement
-        if future_price >= current_price + margin_per_share:
-            targets[i] = [1, 0, 0]  # Buy
-        elif future_price <= current_price - margin_per_share:
-            targets[i] = [0, 0, 1]  # Sell
-        else:
-            targets[i] = [0, 1, 0]  # Hold
-
-    return np.array(training_data), targets[window_size:]
-
-
+from get_stock_data import get_stock_data
+from basic_measurements import calc_moving_average, calc_volatility, add_indicators
+from plots import basic_candlestick_plot, plot_basic_measurements
+from neural_network import NeuralNetwork, create_targets
 
 if __name__ == "__main__":
     chosen_tickers = ["TSLA"]
@@ -37,7 +12,12 @@ if __name__ == "__main__":
     end = "2025-01-01"
 
     stock_data = get_stock_data(chosen_tickers, start, end)
-    for ticker, data in stock_data.items():
-        training_data, targets = create_targets(data)
+    ticker = "TSLA"
+    data = stock_data[ticker]
+    training_data, targets = create_targets(data)
 
 
+    my_nn = NeuralNetwork(input_size=len(training_data[0])) # len of one set of training data
+    my_nn.train(training_data, targets=targets)
+
+    
--- neural_network.py
+++ neural_network.py
@@ -11,7 +11,7 @@ class NeuralNetwork:
 
         # Creates a (input_size by hidden_layers[0]) matrix, 768 x 512 unless otherwise specified
         # Each row is a set of weights for one neuron
-        self.weights.append(0.01 * np.random.randn(input_size, hidden_layers[0]))
+        self.weights.append(0.0001 * np.random.randn(input_size, hidden_layers[0]))
         self.biases.append(np.zeros((1, hidden_layers[0])))
 
         # Hidden layers
@@ -24,19 +24,23 @@ class NeuralNetwork:
         self.biases.append(np.zeros((1, output_size)))
 
     # activation function
-    def relu(self, activation):
-        return np.maximum(0, activation)
-    
-    def relu_derivative(x):
-        return 1 if x > 0 else 0
+    def tanh(self, activation):
+        return np.tanh(activation)
+        
+    def tanh_derivative(self, x):
+        return 1 - np.tanh(x) ** 2
+
  
     def forward(self, inputs):
         # layers contains all activations
-        self.activations = [inputs]
+        self.activations = [inputs.reshape(1, -1)]
 
         for i in range(len(self.weights)):
-            z = np.dot(self.activations[-1], self.weights[i]) + self.biases[i]
-            a = self.relu(z)
+            x = np.array(self.activations[-1].reshape(1, -1))
+            w = np.array(self.weights[i])
+
+            z = np.dot(x, w) + self.biases[i]
+            a = self.tanh(z)
             self.activations.append(a)
 
         return self.activations
@@ -49,7 +53,7 @@ class NeuralNetwork:
         # error = MSE, therefore doutput/da = 2 * error
         delta = 2 * (self.activations[-1] - targets)
         # da / dz
-        delta *= np.vectorize(self.relu_derivative)(self.activations[-1])
+        delta *= np.vectorize(self.tanh_derivative)(self.activations[-1])
 
         for i in reversed(range(len(self.weights))):
             # dz / dw
@@ -58,7 +62,7 @@ class NeuralNetwork:
             bias_gradients[i] = np.sum(delta, axis=0)
 
             if i > 0:  # Skip for the input layer
-                delta = np.dot(delta, self.weights[i].T) * np.vectorize(self.relu_derivative)(self.activations[i])
+                delta = np.dot(delta, self.weights[i].T) * np.vectorize(self.tanh_derivative)(self.activations[i])
 
         return weight_gradients, bias_gradients
 
@@ -85,7 +89,6 @@ class NeuralNetwork:
             self.biases[i] -= learning_rate * averaged_bias_gradients[i]
         
     def train(self, data, targets, epochs=10, batch_size=32):
-        # The real challenge is to evaluate if the market really is bullish or bearish
         num_examples = len(data)
         for epoch in range(epochs):
             permutation = np.random.permutation(num_examples)
@@ -96,21 +99,58 @@ class NeuralNetwork:
                 minibatch_data = shuffled_data[i:i + batch_size]
                 minibatch_targets = shuffled_targets[i:i + batch_size]
 
-                # Hardcoding learning rate to
+                # Hardcoding learning rate
                 # Would be cool with changing learning rate after how steep the gradient is
-                self.backpropagation(minibatch_data, minibatch_targets, learning_rate=0.01)
+                self.backpropagation(minibatch_data, minibatch_targets, learning_rate=0.0001)
 
-            predictions = self.predict(data)
-            accuracy = self.evalute(predictions, targets)
-            print(f"Epoch {epoch}/{epochs}, Accuracy: {accuracy}")
+            # print(f"targets: {targets}")
+            # print(f"targets[epoch]: {targets[epoch]}")
+            # if epoch == 0 or epoch == epochs:
+
+            prediction = self.predict(data[epoch])
+
+            accuracy = self.evaluate(prediction, targets[epoch])
+            print(f"Epoch {epoch + 1}/{epochs}, Accuracy: {accuracy}\n\n")
 
     def predict(self, data):
         return self.forward(data)[-1]
 
     def evaluate(self, predictions, targets):
+        predictions = predictions.reshape(1, -1)
+        targets = targets.reshape(1, -1)
+
+        print(f"predictions: {predictions}")
+        print(f"targets: {targets}\n\n")
+
         predicted_classes = np.argmax(predictions, axis=1)
         true_classes = np.argmax(targets, axis=1)
 
         grades = np.where(predicted_classes == true_classes, 1, 0)
         accuracy = np.mean(grades)
         return accuracy
+
+
+def create_targets(data, margin_per_share=20, horizon=14):
+    window_size = horizon * 10
+    num_targets = len(data["Close"]) - horizon
+    input_size = window_size + 1  # Includes the current price
+    training_data = np.zeros((num_targets - window_size, input_size))
+    targets = np.zeros((num_targets - window_size, 3))  # [buy, hold, sell]
+
+    for i in range(window_size, num_targets):
+        training_window = data["Close"].iloc[i - window_size:i + 1].values.astype(float)
+        training_data[i - window_size] = training_window
+
+        current_price = float(data["Close"].iloc[i]) 
+        future_price = float(data["Close"].iloc[i + horizon])
+
+        # Calculate target based on price movement
+        if future_price >= current_price + margin_per_share:
+            targets[i - window_size] = np.array([1, 0, 0])  # Buy
+        elif future_price <= current_price - margin_per_share:
+            targets[i  - window_size] = np.array([0, 0, 1])  # Sell
+        else:
+            targets[i  - window_size] = np.array([0, 1, 0])  # Hold
+
+
+    return np.array(training_data), targets
